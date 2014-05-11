3c0e3a0ecaeb8f22efe4d14fe0c7ad56bbc6371c
gabesz99 <ruczgabor2@gmail.com>
2014-05-11 12:05:38 +0200

    Tartalmaz

---
+++ Chat/Chat/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(490, 155);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 216);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(202, 43);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Elküld";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(52, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(92, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.ora);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Frissít";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(334, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 43);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Töröl";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Név:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Üzenet:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Chat progi";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Chat/Chat/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace Chat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.Text = Properties.Settings.Default.Nev;
+        }
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Nev = textBox3.Text;
+            Properties.Settings.Default.Save();
+        }
+        private MySqlConnection kapcs = new MySqlConnection();
+        private string kapcsString2 = ConfigurationManager.ConnectionStrings["chatString"].ToString();
+        private StringBuilder olvaso = new StringBuilder();
+        string ido = "";
+        bool elso = true;
+        int id=0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(kapcsString2);
+            string nev = textBox3.Text;
+            kapcs.ConnectionString = kapcsString2;
+
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = kapcs;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "INSERT INTO Beszelgetes (uzenet,felado,ido) VALUES(@uzenet,@felado,@ido)" ;
+            command.Parameters.Add("@uzenet", MySqlDbType.VarChar, 40).Value=textBox2.Text;
+            command.Parameters.Add("@felado", MySqlDbType.VarChar, 40).Value=textBox3.Text;
+            command.Parameters.Add("@ido", MySqlDbType.VarChar, 40).Value = DateTime.UtcNow.ToString("HH:mm:ss");//"hh:mm:ss"
+            command.Connection.Open();
+            //command.ExecuteNonQuery();
+            command.ExecuteScalar();
+
+            //textBox1.Text = "sikerült";
+            command.Connection.Close();
+            //lekerElso();
+            textBox2.Clear();
+            textBox2.Text = "";
+        }
+
+        private void lekerElso(){
+            kapcs.ConnectionString = kapcsString2;
+
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = kapcs;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT ID,ido,felado,uzenet FROM Beszelgetes";
+            command.Connection.Open();
+            MySqlDataReader reader = command.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                id = int.Parse(reader[0].ToString());
+                for (int i = 1; i < reader.FieldCount; i++)
+                {
+                    olvaso.Append(reader[i].ToString() + '\t');
+                    
+                }
+                olvaso.Append(Environment.NewLine);
+                ido = reader[0].ToString();
+            }
+            command.Connection.Close();
+            reader.Close();
+            //olvaso.Append("Masodik");
+            textBox1.Text = olvaso.ToString();
+            //textBox2.Text = ido;
+            //textBox1.ScrollToCaret();
+            textBox1.SelectionStart = textBox1.TextLength;
+            textBox1.ScrollToCaret();
+            //maxid();
+            textBox2.Focus();
+        }
+        private void lekerFolyamat()
+        {
+            kapcs.ConnectionString = kapcsString2;
+
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = kapcs;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT id,ido,felado,uzenet FROM Beszelgetes WHERE id>"+id.ToString();
+            command.Connection.Open();
+            MySqlDataReader reader = command.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                id = int.Parse(reader[0].ToString());
+                for (int i = 1; i < reader.FieldCount; i++)
+                {
+                    olvaso.Append(reader[i].ToString() + '\t');
+
+                }
+                olvaso.Append(Environment.NewLine);
+                ido = reader[0].ToString();
+            }
+            command.Connection.Close();
+            reader.Close();
+            //olvaso.Append("kövi");
+            textBox1.Text = olvaso.ToString();
+            textBox1.SelectionStart = textBox1.TextLength;
+            textBox1.ScrollToCaret();
+            //maxid();
+        }
+        private void ora(object sender, EventArgs e)
+        {
+            if (elso)
+            {
+                lekerElso();
+                //MessageBox.Show("elso");
+                elso = false;
+            }
+            else
+            {
+                //MessageBox.Show("Masodik");
+                lekerFolyamat();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lekerFolyamat();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            olvaso.Clear();
+        }
+        private void maxid()
+        {
+            kapcs.ConnectionString = kapcsString2;
+
+            MySqlCommand command = new MySqlCommand();
+            command.Connection = kapcs;
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT max(id) FROM Beszelgetes ";
+            command.Connection.Open();
+            id = (int)command.ExecuteScalar();
+            command.Connection.Close();
+        }
+        private void enter(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                SendKeys.Send("{BACKSPACE}");
+                button1.PerformClick();
+                textBox2.Clear();
+                textBox2.Text = "";
+            }
+        }
+    }
+}
